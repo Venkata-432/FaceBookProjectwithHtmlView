2fb70a67019e8ff5487c8320bc97bfcb8c423918
vreddyongolu <vreddyongolu@USHYDVREDDYON01.us.deloitte.com>
2022-07-21 17:07:42 +0530

    Pushing facebook project with normal html view

---
+++ facebook/src/main/java/com/facebook/dao/FacebookDAO.java
@@ -0,0 +1,38 @@
+package com.facebook.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import com.facebook.entity.FacebookUser;
+
+public class FacebookDAO {
+
+	public int createProfileDAO(FacebookUser fb) {
+			int i=0;
+			
+			try {
+				//step 1 load driver
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				//step 2 create connection with database
+				Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","admin");
+				
+				//step 3 create query
+				PreparedStatement ps=con.prepareStatement("insert into deloittefacebook values(?,?,?,?)");
+				ps.setString(1, fb.getName());
+				ps.setString(2, fb.getPassword());
+				ps.setString(3, fb.getEmail());
+				ps.setString(4, fb.getAddress());
+				
+				//execute query
+				i=ps.executeUpdate();
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			
+			return i;
+		}
+
+}
---
+++ facebook/src/main/java/com/facebook/view/FacebookView.java
@@ -0,0 +1,78 @@
+package com.facebook.view;
+
+import java.util.Scanner;
+
+import com.facebook.controller.FacebookController;
+
+public class FacebookView {
+
+	public static void main(String[] args) {
+		
+		String ss="y";
+		
+		while(ss.equals("y")) {
+		
+		System.out.println("********************MAIN MENU****************");
+		System.out.println("press 1 to create profile");
+		System.out.println("press 2 to view profile");
+		System.out.println("press 3 to delete profile");
+		System.out.println("press 4 to search profile");
+		System.out.println("press 5 to view all profile");
+		System.out.println("press 6 to edit profile");
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter your choice");
+		int ch = sc.nextInt();
+		
+		FacebookController f=new FacebookController();
+
+		switch (ch) {
+		case 1:  f.createProfile();
+			
+			break;
+		case 2: f.viewProfile();
+			
+			break;
+		case 3:f.deleteProfile();
+			
+			break;
+		case 4:f.searchProfile();
+			
+			break;
+		case 5:f.viewAllProfile();
+			
+			break;
+		case 6:f.editProfile();
+			
+			break;
+		default:
+			System.out.println("wrong choice");
+
+		}
+		System.out.println("do you want to continue press y/n");
+		ss=sc.next();
+		}
+
+	}
+	
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
